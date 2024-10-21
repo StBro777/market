98bc1363c7169a9e7415e18cd2a3381dcbc3e917
stBro@mail.ru <stBro@mail.ru>
2024-10-21 15:38:32 +0300

    After adding fixtures for goods

--- main/views.py
+++ main/views.py
@@ -1,11 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from goods.models import Categories
+
 
 def index(request):
+
+    categories = Categories.objects.all()
+
     context: dict = {
         "title": "Home - Главная",
         "content": "Магазин мебели В гостях у StBro",
+        "categories": categories,
     }
     return render(request, "main/index.html", context)
 
