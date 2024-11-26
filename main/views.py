7aa9d66b6e5138d912d9fd4226a617b43e7db611
stBro@mail.ru <stBro@mail.ru>
2024-11-26 16:48:46 +0300

    in the main and goods applications, I changed the functions to classes

--- main/views.py
+++ main/views.py
@@ -1,22 +1,27 @@
+from string import Template
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 from goods.models import Categories
 
 
-def index(request):
-    context = {
-        "title": "Home - Главная",
-        "content": "Магазин мебели В гостях у StBro",
-    }
+class IndexView(TemplateView):
+    template_name = "main/index.html"
 
-    return render(request, "main/index.html", context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Home - Главная"
+        context["content"] = "Магазин мебели В гостях у St Bro"
+        return context
 
 
-def about(request):
-    context: dict = {
-        "title": "Home - О нас",
-        "content": "О нас",
-        "text_on_page": "Текст про хороший магазин",
-    }
-    return render(request, "main/about.html", context)
+class AboutView(TemplateView):
+    template_name = "main/about.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Home - О нас"
+        context["content"] = "О нас"
+        context["text_on_page"] = "Самый лучший магазин"
+        return context
